1933d2c619bf7903c25c20b5c5d620f5f4cf1f09
Hai.Nguyen <haingdc@gmail.com>
2024-10-20 19:09:46 +0700

    Enhance application with blog API integration in main.ts, add environment support with .env files, and update dependencies

--- main.ts
+++ main.ts
@@ -1,61 +1,33 @@
-// import { Application, Router } from "@oak/oak";
-// import { oakCors } from "@tajpouria/cors";
 import data from "./api/data.json" with { type: "json" };
 // import routeStaticFilesFrom from "./util/routeStaticFilesFrom.ts";
 // @deno-types="@types/express"
 import express from "npm:express"; // Sử dụng npm: prefix
 import cors from "npm:cors";
-
-// const router = new Router();
-
-// router.get("/api/dinosaurs", (context) => {
-//   context.response.body = data;
-// });
-
-// router.get("/api/dinosaurs/:dinosaur", (context) => {
-//   if (!context?.params?.dinosaur) {
-//     context.response.body = "No dinosaur name provided.";
-//   }
-
-//   const dinosaur = data.find((item) =>
-//     item.name.toLowerCase() === context.params.dinosaur.toLowerCase()
-//   );
-
-//   context.response.body = dinosaur ? dinosaur : "No dinosaur found.";
-// });
-
-// const app = new Application();
-// app.use(oakCors());
-// app.use(router.routes());
-// app.use(router.allowedMethods());
-// app.use(routeStaticFilesFrom([
-//   `${Deno.cwd()}/dist`,
-//   `${Deno.cwd()}/public`,
-// ]));
-
-// await app.listen({ port: 8000 });
+import "@std/dotenv"
+import type { Root } from "./types/post.ts";
+import { getApiPosts } from "./util/posts/index.ts";
+import { countPostsByMonth } from "./util/posts/index.ts";
 
 const app = express();
 
 // Cấu hình CORS
 const corsOptions = {
   origin: [
-      "https://www.example.com",
-      "https://pedals-analyze-joyney.deno.dev",
-      "http://localhost:3000" // Thêm localhost để phát triển cục bộ
+    "https://www.example.com",
+    "https://pedals-analyze-joyney.deno.dev",
+    "http://localhost:8080",
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type"],
 };
 
-// Sử dụng middleware CORS
 app.use(cors(corsOptions));
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Dinosaur API!");
+app.get("/", (_req, res) => {
+  res.send("Welcome to the Pedal Analyze Backend");
 });
 
-app.get("/api", (req, res) => {
+app.get("/api", (_req, res) => {
   res.send(data);
 });
 
@@ -70,6 +42,28 @@ app.get("/api/dinosaurs/:dinosaur", (req, res) => {
       res.send("No dinosaurs found.");
     }
   }
+});
+
+app.get("/api/blog-deprecated", async (_req, res) => {
+  try {
+    const response = await fetch(`${Deno.env.get("BLOG_DOMAIN")}/api/blog`); // Gọi API từ localhost
+    const data: Root = await response.json(); // Chuyển đổi response thành JSON
+    res.json(data); // Trả dữ liệu đó về cho client
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Failed to fetch data' }); // Trả về lỗi nếu có
+  }
+});
+
+app.get("/api/blog", async (_req, res) => {
+  const posts = await getApiPosts();
+  if (posts.err) {
+    res.status(500).json({ error: posts.val });
+    return;
+  }
+
+  const postsByMonth = countPostsByMonth(posts.val.posts);
+  res.json(postsByMonth);
 });
 
 app.listen(8000, () => {
---
+++ util/posts/index.ts
@@ -0,0 +1,46 @@
+import type { CountPostsByMonth, Post, Root } from "../../types/post.ts";
+import { Result, Ok, Err } from "ts-results";
+
+/*
+{ "2024-10": 3, "2024-09": 5 }
+
+*/
+
+function countPostsByMonth(posts: Post[]): CountPostsByMonth[] {
+  const counts: Record<string, CountPostsByMonth> = {};
+  
+  posts.forEach(post => {
+      // Chuyển đổi ngày thành đối tượng Date
+      const date = new Date(post.date);
+      const monthKey = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}`; // YYYY-MM
+      const month = date.toLocaleString('default', { month: 'long' });
+
+      // Tăng số lượng bài viết cho tháng đó
+      if (!counts[monthKey]) {
+          counts[monthKey] = { date: monthKey, count: 0, month };
+      }
+      counts[monthKey].count++;
+  });
+
+  let countList: CountPostsByMonth[] = [];
+  if (Object.keys(counts).length) {
+    countList = Object.entries(counts).map(([_key, value]) => value);
+  }
+
+  return countList;
+};
+
+async function getApiPosts(): Promise<Result<Root, string[]>> {
+  try {
+    const response = await fetch(`${Deno.env.get("BLOG_DOMAIN")}/api/blog`); // Gọi API từ localhost
+    const data: Root = await response.json();
+    return new Ok(data);
+  } catch (_error) {
+    return new Err(['Failed to fetch data']);
+  }
+}
+
+export {
+  countPostsByMonth,
+  getApiPosts,
+}
